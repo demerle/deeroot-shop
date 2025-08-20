393c00e6639862ad65da7524724dc91a8d1f6b21
demerle <danny.emerle@gmail.com>
2025-08-20 10:13:42 -0400

    Small style changes and added descriptive text to welcome box

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -6,6 +6,8 @@ export default function Welcome(){
     return (
         <div className="welcome">
             <h1>Welcome to DeerootShop!</h1>
+            <h3 className={"welcome-h3"}>This is my personal store for my youtube content</h3>
+            <br/>
             <div className="solo-text-container about">
                 <Link className="solo-text" to={"/about"}><h2>About Me</h2></Link>
             </div>
